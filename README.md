43398db42ab385b3e0ff07d1d10ef48871a72dc0
Filipe Linhares <filipelinhares@outlook.com>
2014-07-31 20:26:37 -0300

    Release experimental version

--- README.md
+++ README.md
@@ -1,5 +1,46 @@
 # ConciseCSS - Generator
 > The best way for you start your project!  
 
-# License
+## Experimental version
+This is an experimental version of Concise generator.
+
+## Usage
+
+Meke sure you have [Node.js](http://nodejs.org) and NPM.
+
+Clone the repo
+```
+git clone git@github.com:ConciseCSS/concise-generator.git concise-generator
+cd concise-generator
+[sudo] npm link
+```
+
+Now you can use this normaly.
+
+`concise [folder]`
+
+It's generate:
+
+```
+myproject
+ |
+ |-js
+ | |-concise.min.js
+ |-css
+ | |-concise.min.css
+ |-image
+ |-index.html
+ |
+```
+
+## Options
+```
+    -h, --help     output usage information
+    -V, --version  output the version number
+    -p, --prod     use production version
+```
+
+Any issue or pull request follow the Concise [contributing](https://github.com/ConciseCSS/concise.css/blob/master/CONTRIBUTING.md) guide.
+
+## License
 MIT
---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+'use strict';
+
+var program = require('commander');
+var path = require('path');
+var fs = require('fs');
+
+var util = require('./lib/util');
+var pkg = require('./package');
+
+program
+  .usage('[folder]')
+  .version(pkg.version)
+  .option('-p, --prod', 'use production version')
+  .parse(process.argv);
+
+var destinationPath = program.args.shift() || '.';
+
+var index   = fs.readFileSync(__dirname + '/templates/index.html');
+var css     = fs.readFileSync(__dirname + '/templates/concise-css/concise.css')
+var cssMin  = fs.readFileSync(__dirname + '/templates/concise-css/concise.min.css')
+var js      = fs.readFileSync(__dirname + '/templates/js/concise.js');
+var jsMin   = fs.readFileSync(__dirname + '/templates/js/concise.min.js');
+
+(function generateApp(path) {
+  util.emptyDirectory(path, function(empty) {
+    if (empty || program.force) {
+      createApp(path);
+    } else {
+      program.confirm('destination path is not empty, are you sure to continue?', function(ok) {
+        if (ok) {
+          process.stdin.destroy();
+          createApp(path);
+        } else {
+          util.abort('Process canceled!')
+        }
+      });
+    }
+  });
+})(destinationPath);
+
+function createApp(path) {
+  util.mkdir(path, function() {
+    util.mkdir(path + '/css');
+    util.mkdir(path + '/images');
+    util.mkdir(path + '/js');
+
+    util.write(path + '/index.html', index);
+
+    if (program.prod) {
+      util.write(path + '/css/concise.css', css)
+      util.write(path + '/js/concise.js', js);
+    } else {
+      util.write(path + '/css/concise.min.css', cssMin)
+      util.write(path + '/js/concise.min.js', jsMin);
+    }
+  });
+}
